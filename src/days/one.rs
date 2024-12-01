d4c7458c31934baf8581d0805bd616893517083d
Yupiel <stefan_mitrovic@outlook.com>
2024-12-01 12:42:46 +0100

    slight update day 1

--- src/days/one.rs
+++ src/days/one.rs
@@ -1,32 +1,27 @@
 use std::fs::read_to_string;
 
 pub fn run() -> Result<(), ()> {
-    let input_lines: Vec<String> = read_to_string("src/inputs/day_1")
+    let (mut old_location_ids, mut new_location_ids) = read_to_string("src/inputs/day_1")
         .unwrap()
         .lines()
-        .map(String::from)
-        .collect();
+        .map(|line| {
+            let values: Vec<&str> = line.split("   ").collect();
+            (
+                values[0].parse::<i32>().unwrap(),
+                values[1].parse::<i32>().unwrap(),
+            )
+        })
+        .collect::<(Vec<i32>, Vec<i32>)>();
 
-    let mut left_side = Vec::<i32>::new();
-    let mut right_side = Vec::<i32>::new();
-
-    for line in input_lines {
-        let numberinos: Vec<&str> = line.split("   ").collect();
-
-        left_side.push(numberinos[0].parse().unwrap());
-        right_side.push(numberinos[1].parse().unwrap());
-    }
-
-    left_side.sort();
-    right_side.sort();
+    old_location_ids.sort();
+    new_location_ids.sort();
 
     let mut distances = Vec::<i32>::new();
     let mut similarity = Vec::<i32>::new();
 
-    for (left, right) in left_side.iter().zip(right_side.iter()) {
+    for (left, right) in old_location_ids.iter().zip(new_location_ids.iter()) {
         distances.push(left.abs_diff(*right) as i32);
-
-        similarity.push(left * right_side.iter().filter(|x| *x == left).count() as i32);
+        similarity.push(left * new_location_ids.iter().filter(|x| *x == left).count() as i32);
     }
 
     println!("Day one - Part one: {}", distances.iter().sum::<i32>());
